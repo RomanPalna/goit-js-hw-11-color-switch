b3c102000af146ece649ca08afc4291aced4cd52
Roman Palna <roma.palna@gmail.com>
2020-10-28 20:48:25 +0200

    xc

--- app.js
+++ app.js
@@ -8,60 +8,56 @@ const colors = [
 ];
 
 const refs = {
-    body: document.querySelector('.js-switcher'),
-    startBtn: document.querySelector('.js-start'),
-    stopBtn: document.querySelector('.js-stop')
-}
+  body: document.querySelector('.js-switcher'),
+  startBtn: document.querySelector('.js-start'),
+  stopBtn: document.querySelector('.js-stop'),
+};
 
 refs.startBtn.addEventListener('click', () => {
-  timer.start()
-  })
+  timer.start();
+});
 
 refs.stopBtn.addEventListener('click', () => {
-  timer.stop()
-})
+  timer.stop();
+});
 
 const timer = {
   timerId: null,
   isActive: false,
-  
-  
+
   start() {
     if (this.isActive) {
-      return
+      return;
     }
-    refs.startBtn.setAttribute('disabled', 'disabled')
-    refs.stopBtn.removeAttribute('disabled')
-    this.isActive = true
+    refs.startBtn.setAttribute('disabled', 'disabled');
+    refs.stopBtn.removeAttribute('disabled');
+    this.isActive = true;
     this.timerId = setInterval(() => {
-      const promises = colors.map(switchColors)
-      Promise.race(promises).then(({ color }) => refs.body.style.backgroundColor = color)
-    }, 1000)
-   
+      const promises = colors.map(switchColors);
+      Promise.race(promises).then(
+        ({ color }) => (refs.body.style.backgroundColor = color),
+      );
+    }, 1000);
   },
 
   stop() {
-    this.isActive = false
-    clearInterval(this.timerId)
-    refs.startBtn.removeAttribute('disabled')
-    refs.stopBtn.setAttribute('disabled', 'disabled')
-  }
-}
+    this.isActive = false;
+    clearInterval(this.timerId);
+    refs.startBtn.removeAttribute('disabled');
+    refs.stopBtn.setAttribute('disabled', 'disabled');
+  },
+};
 
 function switchColors(color) {
-  return new Promise((resolve) => {
-   const time = randomIntegerFromInterval(500, 1000) 
+  return new Promise(resolve => {
+    const time = randomIntegerFromInterval(500, 1000);
 
     setInterval(() => {
-      resolve({ color })
-      
-    }, time)
+      resolve({ color });
+    }, time);
   });
 }
 
 const randomIntegerFromInterval = (min, max) => {
   return Math.floor(Math.random() * (max - min + 1) + min);
 };
-
-
-
